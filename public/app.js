5e69168c83ea361726672b1d553aa0b6eb7f084f
DanielLeoPUC <daniel.leo@sga.pucminas.br>
2025-10-10 21:04:30 +0000

    adicionar projeto inicial

---
+++ public/app.js
@@ -0,0 +1,195 @@
+
+const carrousel = [{
+  "id": 1,
+  "imagem": "img/noiteestrelada.jpg",
+  "titulo": "Noite estrelada"
+
+}]
+
+const obras = [{
+
+  "id": 0,
+  "imagem": "img/avinha.jpg",
+  "titulo": "A Vinha Encarnada",
+  "ano": "1888",
+  "descricao": " A Vinha Encarnada é um quadro do pintor holandês Vincent Van Gogh, concluído no início de novembro de 1888.<br>&nbsp; Esta foi a única peça vendida pelo artista em vida. O quadro foi adquirido em Bruxelas por 400 Francos, por Anna Boch.",
+  "descricaoCompleta": "O quadro do pintor holandês Vincent Van Gogh, concluído no início de novembro de 1888. Esta foi a única peça vendida pelo artista em vida.<br>&nbspO quadro foi adquirido em Bruxelas por 400 Francos, por Anna Boch. Van Gogh ficou sabendo do negócio por intermédio de seu irmão e mecenas Theo Van Gogh, em fevereiro de 1890.<br>&nbsp A Vinha Encarnada foi exibida pela primeira vez na mostra de Les XX, em 1890, em Bruxelas e vendida por 400 Francos belgas  para Anna Boch, uma pintora impressionista da Bélgica, membra do Les XX e colecionadora de arte. Anna era irmã de Eugène Boch, também pintor impressionista e amigo de Van Gogh, que havia pintado o retrato de Boch  em Arles, no outono de 1888.<br>&nbsp Assim como o Terraço do Café à Noite, foi adquirida pelo colecionador russo Sergei Shchukin. Veio a ser nacionalizada pelos bolcheviques junto com o resto da coleção de Sergei, a qual, eventualmente, foi para o Museu Pushkin de Belas Artes em Moscou."
+},
+{
+  "id": 1,
+  "imagem": "img/giracois.jpg",
+  "titulo": "Os Doze girassóis numa jarra",
+  "ano": "1888",
+  "descricao": "Van Gogh pintou uma série de 11 quadros de girassóis (quatro em Paris e sete em Arles), que se tornaram algumas das suas obras mais icónicas, simbolizando para ele felicidade, gratidão, força e resistência.",
+  "descricaoCompleta": "Os Doze girassóis numa jarra é uma pintura do pintor holandês Vincent van Gogh. Após a chegada do pintor ao sul da França, estabelecendo-se em Arles, Van Gogh passou a utilizar efeitos de cores e de luz com mais intensidade. Doze Girassóis numa Jarra pode ser considerado o culminar de todo este efeito em na obra do artista.<br>&nbsp Finalizado em agosto de 1888, o quadro está hoje exposto na Neue Pinakothek, em Munique.<br>&nbsp  Atualmente, esta é uma das telas mais famosas do mundo. Tal sucesso e reconhecimento contrastam com a vida do seu autor, que sempre viveu à margem da sociedade. Ao longo de toda a trajetória de Van Gogh, o artista vendeu somente um quadro. Ele só foi reconhecido mundialmente depois de sua morte."
+},
+{
+  "id": 2,
+  "imagem": "img/noiteestrelada.jpg",
+  "titulo": "Noite Estrelada Sobre o Ródano",
+  "ano": "1889",
+  "descricao": "  A Noite Estrelada é uma pintura de Vincent van Gogh de 1889. A obra retrata a vista da janela de um quarto do hospício de Saint-Rémy-de-Provence, pouco antes do nascer do sol, com a adição de um vilarejo idealizado pelo artista.<br>&nbsp; A tela faz parte da coleção permanente do Museu de Arte Moderna de Nova Iorque desde 1941.",
+  "descricaoCompleta": " Noite Estrelada Sobre o Ródano é uma pintura de 1888 do pintor holandês Vincent van Gogh.O quadro foi pintado após a sua mudança para Arles, no sul da França, nove meses antes de ser hospitalizado e dois anos antes de cometer suicídio. Em Arles, Van Gogh produziu outras obras famosas, como Quarto em Arles e Terraço do Café à Noite.<br>&nbsp;Atualmente a obra está exposta no Musée d'Orsay, em Paris, França. Ela foi exposta pela primeira vez em 1889 Salon des Artistes Independants.<br>&nbsp;A obra retrata a paisagem vista à noite à beira do Ródano, um importante rio europeu."
+},
+{
+  "id": 3,
+  "imagem": "img/autoretrato.jpg",
+  "titulo": "Autorretrato",
+  "ano": "1889",
+  "descricao": " O Autorretrato com Chapéu de Feltro é um óleo sobre tela, pintado por Vincent van Gogh em 1888.<br>&nbsp; Van Gogh pintou esta tela no inverno europeu de 1887-1888, quando viveu em Paris por dois anos.",
+  "descricaoCompleta": "O pintor holandês pós-impressionista Vincent van Gogh pintou um autorretrato em óleo sobre tela em setembro de 1889. A obra, que pode ter sido o último autorretrato de Van Gogh, foi pintada pouco antes de ele deixar Saint-Rémy-de-Provence, no sul da França. A pintura está agora no Musée d'Orsay, em Paris.<br>&nbsp;Este autorretrato foi um dos cerca de 32 produzidos ao longo de um período de 10 anos, e estes foram uma parte importante de seu trabalho como pintor.Ele pintou a si mesmo porque muitas vezes não tinha dinheiro para pagar modelos. Ele levou a pintura com ele para Auvers-sur-Oise, perto de Paris, onde a mostrou ao Dr. Paul Gachet, que a achou maravilhosa.<br>&nbsp; Os historiadores da arte estão divididos sobre se esta pintura ou o Autorretrato sem barba é o autorretrato final de Van Gogh. Os historiadores de arte Ingo F. Walther e Jan Hulsker consideram que esta é a última, com Hulsker considerando que foi pintada em Arles após a internação de Van Gogh no hospital depois de mutilar sua orelha, enquanto Ronald Pickvance acha que Autorretrato sem barba foi a pintura posterior"
+},
+{
+  "id": 4,
+  "imagem": "img/vaso.jpg",
+  "titulo": "Vaso com Iríses",
+  "ano": "1890 ",
+  "descricao": "Vaso com íris contra um fundo amarelo é uma pintura a óleo sobre tela feita em 1889 pelo pintor Vincent Van Gogh. Está preservado no Museu Van Gogh em Amsterdã.<br> &nbsp; É um dos trabalhos realizados enquanto ele estava internado na clínica psiquiátrica de Saint-Rémy, cidade próxima a Arles.",
+  "descricaoCompleta": "Vaso com Íris Contra um Fundo Amarelo é uma pintura a óleo sobre tela feita em 1889 pelo pintor Vincent Van Gogh . Está preservada no Museu Van Gogh em Amsterdã . É uma das obras feitas enquanto ele estava internado na clínica psiquiátrica de Saint-Rémy, uma cidade perto de Arles. A obra está localizada no museu Van Gogh em Amsterdã.<br>&nbsp;Van Gogh tem uma obra semelhante, com o mesmo nome, mas também conhecida como Vaso com Íris , localizada no Metropolitan Museum of Art, em Nova York .<br>&nbsp;A pintura criada em  1890 pertence a série de pinturas sobre flores ( Vaso com Centáureas e Papoulas , Vaso com Rosas Cor-de-Rosa , Vaso Japonês com Rosas e Anêmonas ) pode-se perceber a influência das gravuras japonesas , tema que o fascinou durante a maior parte de sua trajetória artística e que foi muito popular entre a sociedade de sua época, o que o levou a eliminar as sombras e ocupar grande parte da pintura com flores densas e seus caules grossos. "
+},
+{
+  "id": 5,
+  "imagem": "img/oscomedores.jpg",
+  "titulo": "Os Comedores de Batatas",
+  "ano": "1885",
+  "descricao": " Os comedores de batata é um quadro do pintor Vincent van Gogh, terminado em abril de 1885.<br>&nbsp;o retrato de uma época, da classe trabalhadora e sua base alimentar.",
+  "descricaoCompleta": "Os Comedores de Batatas é uma pintura a óleo sobre tela feita em 1885 pelo pintor holandês Vincent van Gogh. A obra está localizada no Museu Van Gogh em Amsterdã. A pintura retrata uma família de camponeses comendo batatas, e é considerada uma das obras mais importantes do período inicial de Van Gogh na Holanda. A obra foi inspirada por um esboço feito por Van Gogh em 1883, durante sua estadia em Nuenen, onde ele morava com seus pais e trabalhava como pastor.<br>&nbsp;Van Gogh queria mostrar a vida difícil dos camponeses e a importância da batata como alimento básico para eles. Ele usou cores escuras e tons terrosos para criar uma atmosfera sombria e realista, enfatizando a dureza da vida rural. A pintura foi criticada na época por sua representação crua e não idealizada dos camponeses, mas hoje é considerada uma obra-prima do pós-impressionismo e um exemplo do compromisso social de Van Gogh."
+}
+
+];
+
+function CarregarLogo() {
+  const logo = document.getElementById("logo");
+  let strTexto = "";
+
+  strTexto +=
+    `<div id="container-logo">
+    <img src="img/logoVanGog.png"></img>
+    </div>`
+  logo.innerHTML = strTexto;
+
+}
+CarregarLogo();
+
+
+function carregarCarrousel() {
+  const carrousel = document.getElementById("carrousel");
+
+  let strTexto = "";
+
+  strTexto = `
+ <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/ceu.png" alt="Céu">
+    </div>
+     <div class="carousel-item">
+       <img class="d-block w-100" src="img/avinha2.jpg" alt="A vinha ">
+    </div>
+    <div class="carousel-item">
+       <img class="d-block w-100" src="img/ceu.png" alt=" O quarto ">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>`
+
+  carrousel.innerHTML = strTexto;
+}
+carregarCarrousel();
+
+
+
+
+
+
+function carregarCards() {
+  const tela = document.getElementById("tela");
+  let strTexto = "";
+
+  for (let i = 0; i < obras.length; i++) {
+    strTexto += `
+      <article class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4   text-center border border-light  p-2">
+        <img src="${obras[i].imagem}" alt="${obras[i].titulo}" class="img-fluid mb-2">
+        <h2>${obras[i].titulo}</h2>
+        <p>${obras[i].descricao}</p>
+        <button  onclick="buscarObras(${obras[i].id})">Ver mais</button>
+      </article>
+    `;
+  }
+
+  tela.innerHTML = strTexto;
+}
+carregarCards();
+
+
+
+function criarFooter() {
+  const footer = document.getElementById("footer")
+  let strTexto = "";
+  strTexto += `
+ <article class="col-12 col-sm-12 col-md-4 col-lg-6 col-xl-4  ">
+ <h3>Sobre mim</h3>
+ <p>&nbsp;Me chamo Daniel Leo, tenho 23 anos e atualmente moro em Ribeirão das Neves.<br>&nbsp; No momento estou cursando o primeiro periodo de  ADS ( Análize e desenvolvimento de sistemas ) na PUC-São Gabriel.
+ <br>&nbsp;Uma das minhas paixões são obras artisticas, especialmente as do pintor Van Gogh que são as minhas favoritas. Nesse site você conhecerá um pouco de suas obras mais fantásticas.<br> Espero que goste!</p>
+ </article>
+  <article class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+  <img src="img/20210215_103822-01.jpeg"></img>
+   <p><strong>Redes Sociais:</strong></p>
+   <i class="fab fa-facebook"></i>
+      <i class="fab fa-github">
+      <i class="fab fa-linkedin"></i></i>
+  </article>
+<article class="col-12 col-sm-12 col-md-2 col-lg-6 col-xl-4">
+   <h3>Autoria</h3>
+   <p><strong>Aluno:</strong>Daniel Junio Léo</p>
+   <p><strong>Curso:</strong>Análise e desenvolvimento de sistemas</p>
+   <p><strong>Turno:</strong>Noite</p>
+    </article>
+
+ `
+  footer.innerHTML = strTexto;
+}
+criarFooter();
+
+
+function buscarObras(id) {
+  window.location.href = `detalhes.html?id=${id}`;
+}
+
+
+function buscarDetalhes() {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("id");
+
+  const obra = obras.find(function (a) {
+    return a.id == id;
+  });
+  const telaDetalhes = document.getElementById("teladetalhes");
+
+  if (obra) {
+    telaDetalhes.innerHTML = `
+      <div class="col-12 text-center " id="d">
+        <img src="${obra.imagem}" alt="imagem" width="400px" heigth="400px" class="img-fluid mb-3 p-3">
+        <h2>${obra.titulo}</h2>
+        <p><strong>Descrição:<br></strong>${obra.descricao}</p>
+        <p><strong>Ano de Criação:<br></strong> ${obra.ano}</p>
+        <p><strong>História:<br></strong>${obra.descricaoCompleta}</p>
+        <a href="index.html"><button id="btnbtn">Home</button>
+      </div>
+    `;
+  } else {
+    telaDetalhes.innerHTML = `<p>ERRO 404 página não encontrada</p>`;
+  }
+}
